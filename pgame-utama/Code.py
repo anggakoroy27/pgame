710c38b31ed029b8d9003d2eb3fc1bd133397c36
anggakoroy27 <56109545+anggakoroy27@users.noreply.github.com>
2019-11-08 00:03:14 +0900

    Tugas 5

---
+++ pgame-utama/Code.py
@@ -0,0 +1,36 @@
+True and True
+True and False
+False or True
+True or True
+not True
+not false
+
+a = 5
+print (a)
+a += 5
+a -= 5
+print (a)
+a /= 5
+a %= 5
+a **= 5
+a //= 5
+print (a)
+
+A = [ 2, 3, 4, 5 ]
+3 in A
+1 in A
+
+A = [ 2, 3, 4, 5 ]
+3 not in A
+1 not in A
+
+a,b = 20,20
+a is b
+a is not b
+aku,kamu="budi","iwan"
+aku is kamu
+aku is "budi"
+
+a,b = 33,44
+a & b
+a | b
